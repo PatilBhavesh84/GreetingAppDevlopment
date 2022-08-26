4687c075478d5721de76c548f50ec5422c74cee0
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-26 13:46:31 +0530

    Added:Welcome message

---
+++ src/main/java/com/bridglabz/greetingappdevlopment/GreetingAppDevlopmentApplication.java
@@ -0,0 +1,14 @@
+package com.bridglabz.greetingappdevlopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevlopmentApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Greeting App Devlopment");
+        SpringApplication.run(GreetingAppDevlopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridglabz/greetingappdevlopment/GreetingAppDevlopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridglabz.greetingappdevlopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppDevlopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
